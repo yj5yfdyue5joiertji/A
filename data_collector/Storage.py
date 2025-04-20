8c4f0f8f8f493a775af1562cd9c460b0fe3f5385
Joabe Capivara <hvjkvhuhukyuk@gmail.com>
2025-04-20 13:30:55 -0300

    Segunda parte concluída: integração com InfluxDB

--- data_collector/Storage.py
+++ data_collector/Storage.py
@@ -1,5 +1,6 @@
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
+from datetime import datetime
 
 class InfluxDBStorage:
     def __init__(self, token, org, bucket, url="http://localhost:8086"):
@@ -21,5 +22,16 @@ class InfluxDBStorage:
         self.write_api.write(bucket=self.bucket, org=self.org, record=point)
 
 # Uso (configure seu token e org):
-# storage = InfluxDBStorage(token="seu_token", org="sua_org", bucket="cripto")
-# storage.save_candle("BTC/USDT", candle)
+storage = InfluxDBStorage(token="ot5KhaV7EK4EB7KZYpmcHZFUqZtrMiUsTxaELEnj_xuJtTmev_fBNoyQCRE6OzFyhOTvie_FQeZuS6yqcxqqzg==", org="Trading bot", bucket="Trading Bot")
+
+candle = {
+    'timestamp': "2025-03-10 01:00:00",
+    'open': 81292.01,
+    'high': 81888.00,
+    'low': 80802.59,
+    'close': 81766.01,
+    'volume': 1700.61474
+}
+
+storage.save_candle("BTC/USDT", candle)
+print("🔥 Candle enviado com sucesso!")
